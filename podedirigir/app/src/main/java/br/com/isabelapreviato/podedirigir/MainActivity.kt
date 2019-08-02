127017348bddf89384162450fb051d38bda2d239
isabelapreviato <isabelapreviato@hotmail.com>
2019-08-02 11:45:46 -0300

    aula 05 02-08-2019

--- podedirigir/app/src/main/java/br/com/isabelapreviato/podedirigir/MainActivity.kt
+++ podedirigir/app/src/main/java/br/com/isabelapreviato/podedirigir/MainActivity.kt
@@ -1,12 +1,26 @@
 package br.com.isabelapreviato.podedirigir
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnLets.setOnClickListener {
+            val nome = edtNome.editableText.toString().trim()
+            if(nome.isNotEmpty()){
+                val intent = Intent(this, ResultadoActivity::class.java)
+                intent.putExtra("nome",nome)
+                startActivity(intent)
+            }
+            else{
+                Toast.makeText(this, "Digite um nome! ♡", Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- podedirigir/app/src/main/java/br/com/isabelapreviato/podedirigir/ResultadoActivity.kt
+++ podedirigir/app/src/main/java/br/com/isabelapreviato/podedirigir/ResultadoActivity.kt
@@ -2,11 +2,40 @@ package br.com.isabelapreviato.podedirigir
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_resultado.*
 
 class ResultadoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resultado)
+
+        val nome = intent.getStringExtra("nome")
+
+        txvAloha.text = "A L O H A,  $nome"
+
+        var listaIdades = arrayListOf<Int>()
+
+        for(idade in 1..100){
+            listaIdades.add(idade)
+        }
+
+        var idadeAdapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, listaIdades)
+
+        spnIdades.adapter = idadeAdapter
+
+
+        btnClique.setOnClickListener {
+            var idadeSelecionada = spnIdades.selectedItem as Int
+
+            if(idadeSelecionada>=18){
+                txvResultado.text = "Pode dirigir!"
+            }
+
+            else{
+                txvResultado.text = "Não pode dirigir!"
+            }
+        }
     }
 }
